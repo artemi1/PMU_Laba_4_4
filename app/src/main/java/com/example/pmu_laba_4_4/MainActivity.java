a1125cf4f81557239168c0bbc673f4d33906d4d1
Artemi1 <Artemkurskov@gmail.com>
2019-11-13 23:59:02 +0300

    v 1.0
    DONE
    - bounced button
    
    TO DO
    - positive objects
    - negative objects
    - collectible bin

--- app/src/main/java/com/example/pmu_laba_4_4/MainActivity.java
+++ app/src/main/java/com/example/pmu_laba_4_4/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.pmu_laba_4_4;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    ImageButton btnRestart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         //final PlayArena playArena = (PlayArena) findViewById(R.id.playArena);
+
+        btnRestart = (ImageButton) findViewById(R.id.btnRestart);
+
+
     }
+
+
+    public void onRestartBtnClick(View view) {
+        final Animation animation = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        BounceInterpolator interpolator = new BounceInterpolator(0.1, 20);
+        animation.setInterpolator(interpolator);
+        btnRestart.startAnimation(animation);
+    }
+
 }
