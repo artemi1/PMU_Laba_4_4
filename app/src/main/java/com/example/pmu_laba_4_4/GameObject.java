f6e16d52b1435e073d63465181a1264d0396289b
Artemi1 <Artemkurskov@gmail.com>
2019-11-16 01:40:57 +0300

    v 1.2 Redesigned to Constraint Layout Demo object move (SLOW!!!)
    DONE
    - bounced button
    
    TO DO
    - positive objects
    - negative objects
    - collectible bin

--- app/src/main/java/com/example/pmu_laba_4_4/GameObject.java
+++ app/src/main/java/com/example/pmu_laba_4_4/GameObject.java
@@ -1,10 +1,72 @@
 package com.example.pmu_laba_4_4;
 
-public class GameObject {
+import android.content.Context;
+import android.transition.TransitionManager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.view.MotionEventCompat;
+
+public class GameObject extends AppCompatImageView {
     //private float prevX, currX, prevY, currY;
-    private float X, Y;
+    private int X, Y;
+    private int prevEventX, prevEventY;
+    //ConstraintLayout parentLayout;
+    ConstraintSet constraintSet;
 
+    public GameObject(Context context){
+        super(context);
 
+        constraintSet = new ConstraintSet();
+    }
+
+    public void initXY (int X, int Y){
+        this.X = X;
+        this.Y = Y;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        int eventX = (int)event.getRawX();
+        int eventY = (int)event.getRawY();
+
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN: // нажатие
+                prevEventX = eventX;
+                prevEventY = eventY;
+
+                break;
+            case MotionEvent.ACTION_MOVE: // движение
+                int deltaX = eventX - prevEventX;
+                int deltaY = eventY - prevEventY;
+
+                prevEventX = eventX;
+                prevEventY = eventY;
+
+                X += deltaX;
+                Y += deltaY;
+
+                ConstraintLayout parentLayout = (ConstraintLayout)getParent();
+                int mID = this.getId();
+                constraintSet.clone(parentLayout);
+                constraintSet.setMargin(mID, ConstraintSet.START, X);
+                constraintSet.setMargin(mID, ConstraintSet.TOP, Y);
+                constraintSet.applyTo(parentLayout);
+
+                break;
+            case MotionEvent.ACTION_UP: // отпускание
+            case MotionEvent.ACTION_CANCEL:
+
+                break;
+        }
+        return true;
+    }
 
 
 }
--- app/src/main/java/com/example/pmu_laba_4_4/MainActivity.java
+++ app/src/main/java/com/example/pmu_laba_4_4/MainActivity.java
@@ -3,27 +3,54 @@ package com.example.pmu_laba_4_4;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
-public class MainActivity extends Activity implements View.OnTouchListener{
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity{
 
     ImageButton btnRestart;
+    ArrayList<GameObject> gameObjects = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //final PlayArena playArena = (PlayArena) findViewById(R.id.playArena);
-
         btnRestart = (ImageButton) findViewById(R.id.btnRestart);
 
+        int x=200;
+        int y=300;
 
+        GameObject mGameObject = new GameObject(this);
+        mGameObject.setImageResource(R.drawable.wolf);
+        mGameObject.setId(View.generateViewId());
+        mGameObject.initXY(x,y);
+
+        ConstraintLayout.LayoutParams lp = new ConstraintLayout.LayoutParams(200, 200);
+        mGameObject.setLayoutParams(lp);
+
+        ConstraintLayout layout = (ConstraintLayout)findViewById(R.id.playArena);
+        layout.addView(mGameObject);
+
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(layout);
+        constraintSet.connect(mGameObject.getId(), ConstraintSet.TOP, layout.getId(), ConstraintSet.TOP, y);
+        constraintSet.connect(mGameObject.getId(), ConstraintSet.LEFT, layout.getId(), ConstraintSet.LEFT, x);
+        constraintSet.applyTo(layout);
+
+
+
+        //gameObjects.add(mGameObject);
     }
 
 
@@ -33,29 +60,6 @@ public class MainActivity extends Activity implements View.OnTouchListener{
         BounceInterpolator interpolator = new BounceInterpolator(0.1, 20);
         animation.setInterpolator(interpolator);
         btnRestart.startAnimation(animation);
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        //x = event.getX();
-        //y = event.getY();
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN: // нажатие
-                //sDown = "Down: " + x + "," + y;
-                //sMove = ""; sUp = "";
-                break;
-            case MotionEvent.ACTION_MOVE: // движение
-                //sMove = "Move: " + x + "," + y;
-                break;
-            case MotionEvent.ACTION_UP: // отпускание
-            case MotionEvent.ACTION_CANCEL:
-                //sMove = "";
-                //sUp = "Up: " + x + "," + y;
-                break;
-        }
-        //tv.setText(sDown + "\n" + sMove + "\n" + sUp);
-        return true;
     }
 
 
